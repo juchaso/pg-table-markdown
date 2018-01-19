a3de51fa4d82c15d8d1677f471ab66ff352d9e29
juchimiuk <juchimiuk@accuratus-software.pl>
2018-01-19 17:39:37 +0100

    fix bug.

--- pg_table_markdown/utils.py
+++ pg_table_markdown/utils.py
@@ -3,9 +3,8 @@ from collections import defaultdict
 
 def parse_schema_data(schema_data):
     output = defaultdict(list)
-    output_td = defaultdict(list)
     for i in schema_data:
         table_name = i.pop('table_name')
         table_description = i.pop('table_description')
-        output[' '.join([table_name,",",table_description])].append(i)
+        output[" ".join([table_name,",",table_description])].append(i)
     return output
