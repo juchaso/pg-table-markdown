f1357dd395f2eb26e8579f15ac9ba04935dc6f7d
juchimiuk <juchimiuk@accuratus-software.pl>
2018-01-19 17:44:11 +0100

    fix bug.

--- pg_table_markdown/utils.py
+++ pg_table_markdown/utils.py
@@ -6,5 +6,7 @@ def parse_schema_data(schema_data):
     for i in schema_data:
         table_name = i.pop('table_name')
         table_description = i.pop('table_description')
-        output[" ".join([table_name,table_description])].append(i)
+        if table_description is None:
+            table_description = 'None'
+        output[" ".join([table_name,',',table_description])].append(i)
     return output
