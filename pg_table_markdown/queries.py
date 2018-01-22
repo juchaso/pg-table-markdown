5af089f1463a99efc7ca8a1ad9a24d671495c76c
juchimiuk <juchimiuk@accuratus-software.pl>
2018-01-22 08:30:58 +0100

    Trim table and columns descriptions..

--- pg_table_markdown/queries.py
+++ pg_table_markdown/queries.py
@@ -1,13 +1,13 @@
 def build_schema_query(table_schema):
     schema_query = """
     SELECT c.table_name,
-        (select pg_catalog.obj_description(oid) from pg_catalog.pg_class cl where cl.relname=c.table_name) as table_description,
+        trim((select pg_catalog.obj_description(oid) from pg_catalog.pg_class cl where cl.relname=c.table_name)) as table_description,
 	    c.column_name,
         c.column_default, 
         c.is_nullable, 
         c.data_type, 
         c.character_maximum_length, 
-        pgd.description  
+        trim(pgd.description) as description
     FROM information_schema.columns c
     JOIN pg_catalog.pg_statio_all_tables as st ON (c.table_schema=st.schemaname and c.table_name=st.relname)
     LEFT JOIN pg_catalog.pg_description pgd on (pgd.objoid=st.relid AND pgd.objsubid=c.ordinal_position)
